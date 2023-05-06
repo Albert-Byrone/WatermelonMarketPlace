6c76753fd2ab84b840f907b1d6c7db8cccde4117
Albert Byrone <albertbyrone1677@gmail.com>
2023-05-06 21:42:14 +0300

    create messages to be displayed based on some actions

---
+++ backend/constants.py
@@ -0,0 +1,4 @@
+SURVEY_CREATED_MESSAGE = "Survey has been created successfully"
+SURVEY_NOT_CREATED_MESSAGE = "Survey has not been created successfully. Something went wrong"
+GET_SURVEY_SUCCESSFUL_MESSAGE = "Tasks have been successfully retrieved"
+GET_SURVEY_FAILED_MESSAGE = "Tasks have not been successfully retrieved"
