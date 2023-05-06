67c3c245f5567b7cf07a8ebb97799ba311dc8c55
Albert Byrone <albertbyrone1677@gmail.com>
2023-05-06 21:43:03 +0300

    create a helper method for getting all  surveys

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,8 @@
+from sqlalchemy.orm import Session
+from model import  Survey
+from schemas import SurveyCreate
+
+
 def create_survey(db: Session, survey: SurveyCreate):
     db_survey = Survey(name=survey.name, price=survey.price, balance=survey.balance, network=survey.network, address=survey.address, email=survey.email)
     db.add(db_survey)
@@ -5,3 +10,7 @@ def create_survey(db: Session, survey: SurveyCreate):
     db.refresh(db_survey)
     return db_survey
 
+
+def get_surveys(db: Session):
+    surveys = db.query(Survey).all()
+    return [Survey( id=survey.id, name=survey.name,price=survey.price,balance=survey.balance, network=survey.network,address=survey.address,email=survey.email)for survey in surveys]
