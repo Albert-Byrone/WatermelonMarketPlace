409929adc24ad641239c5d5a1b5e1b2ac7f305a7
Albert Byrone <albertbyrone1677@gmail.com>
2023-05-06 21:49:31 +0300

    cached files

---
+++ frontend/watermelonfront/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "/index.html",
+  "./src/**/*.{html,js}"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
